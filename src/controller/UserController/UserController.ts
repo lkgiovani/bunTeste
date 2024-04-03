eb2616202ce2125528a6ff6c3c46bbf7bead151b
Giovani souza <lkgiovani@gmail.com>
2024-04-03 15:04:36 +0100

    feat: userController

--- src/controller/UserController/UserController.ts
+++ src/controller/UserController/UserController.ts
@@ -1,12 +1,18 @@
 import type { Request, Response } from "express";
-import { createUserZod } from "../Zod/zodUser";
+
 import { z } from "zod";
 import { UserClass } from "../../model/UserClass/UserClass";
+import {
+  CreateUserZod,
+  DeleteUserZod,
+  EditUserZod,
+  GetByIdUserZod,
+} from "../Zod/zodUser";
 
 class UserController {
   async createUser(req: Request, res: Response) {
     try {
-      const createUser = createUserZod.parse(req.body);
+      const createUser = CreateUserZod.parse(req.body);
 
       const response = await UserClass.create(createUser.name, createUser.age);
 
@@ -27,11 +33,112 @@ class UserController {
       }
     }
   }
+  async getAllUser(req: Request, res: Response) {
+    try {
+      const response = await UserClass.getAll();
 
-  async getAllUser() {}
-  async getByIdUser() {}
-  async editUser() {}
-  async deleteUser() {}
+      if (response.length < 1)
+        return res.status(404).json({ message: "User não encontrado" });
+
+      return res.status(200).json(response);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        // Tratamento de erros de validação
+        if (process.env.DEBUG === "true") {
+          console.error("Erros de validação:", error.errors);
+        }
+
+        return res.status(401).json(error.errors);
+      } else {
+        if (process.env.DEBUG === "true") {
+          console.error("Erros de validação:", error);
+        }
+        return res.status(401).json(error);
+      }
+    }
+  }
+  async getByIdUser(req: Request, res: Response) {
+    try {
+      const getByIdUser = GetByIdUserZod.parse(req.body);
+
+      const response = await UserClass.getById(getByIdUser.id);
+
+      if (!response)
+        return res.status(404).json({ message: "User não encontrado" });
+
+      return res.status(200).json(response);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        // Tratamento de erros de validação
+        if (process.env.DEBUG === "true") {
+          console.error("Erros de validação:", error.errors);
+        }
+
+        return res.status(401).json(error.errors);
+      } else {
+        if (process.env.DEBUG === "true") {
+          console.error("Erros de validação:", error);
+        }
+        return res.status(401).json(error);
+      }
+    }
+  }
+  async editUser(req: Request, res: Response) {
+    try {
+      const createUser = EditUserZod.parse(req.body);
+
+      const response = await UserClass.edit(
+        createUser.id,
+        createUser.name,
+        createUser.age
+      );
+
+      if (!response)
+        return res.status(404).json({ message: "User não encontrado" });
+
+      return res.status(200).json(response);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        // Tratamento de erros de validação
+        if (process.env.DEBUG === "true") {
+          console.error("Erros de validação:", error.errors);
+        }
+
+        return res.status(401).json(error.errors);
+      } else {
+        if (process.env.DEBUG === "true") {
+          console.error("Erros de validação:", error);
+        }
+        return res.status(401).json(error);
+      }
+    }
+  }
+  async deleteUser(req: Request, res: Response) {
+    try {
+      const createUser = DeleteUserZod.parse(req.body);
+
+      const response = await UserClass.delete(createUser.id);
+
+      if (!response)
+        return res.status(404).json({ message: "User não encontrado" });
+
+      return res.status(200).json(response);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        // Tratamento de erros de validação
+        if (process.env.DEBUG === "true") {
+          console.error("Erros de validação:", error.errors);
+        }
+
+        return res.status(401).json(error.errors);
+      } else {
+        if (process.env.DEBUG === "true") {
+          console.error("Erros de validação:", error);
+        }
+        return res.status(401).json(error);
+      }
+    }
+  }
 }
 
 export default new UserController();
--- src/controller/Zod/zodUser.ts
+++ src/controller/Zod/zodUser.ts
@@ -1,6 +1,20 @@
 import { z } from "zod";
 
-export const createUserZod = z.object({
+export const CreateUserZod = z.object({
   name: z.string(),
   age: z.number(),
+});
+
+export const GetByIdUserZod = z.object({
+  id: z.string(),
+});
+
+export const EditUserZod = z.object({
+  id: z.string(),
+  name: z.string(),
+  age: z.number(),
+});
+
+export const DeleteUserZod = z.object({
+  id: z.string(),
 });
